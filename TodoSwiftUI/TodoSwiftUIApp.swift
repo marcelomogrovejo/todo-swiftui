3bc2ff9eebe79e238e7aaa1e9688c10e04cc4a26
Marcelo Mogrovejo <mogro@MogroPC.local>
2023-12-04 00:41:39 +0800

    Initial commit

---
+++ TodoSwiftUI/TodoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoSwiftUIApp.swift
+//  TodoSwiftUI
+//
+//  Created by Marcelo Mogrovejo on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
